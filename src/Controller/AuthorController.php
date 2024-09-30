de543038eb1005644981a0fd4c4844f403a0fbc9
ser.name Your <djebbihaitem9@gmail.com>
2024-09-30 23:07:50 +0100

    second commit

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -16,17 +16,23 @@ class AuthorController extends AbstractController
     //     ]);
     // }
 
-    #[Route('/authorss', name: 'list_authors')]
-    public function listAuthors(): Response
-    {
-        $authors = array(
-            array('id' => 1, 'picture' => '/images/victor hugo.jpeg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100,'age'=> 12),
-            array('id' => 2, 'picture' => '/images/william shekspear.jpeg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200,'age'=> 12),
-            array('id' => 3, 'picture' => '/images/taha hussein.jpeg', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300,'age'=> 12),
-        );
+    #[Route('/author/{id}', name: 'list_authors')]
+    public function authorDetails(int $id): Response
+{
+    $authors = [
+        1 => ['id' => 1, 'picture' => '/images/victor hugo.jpeg', 'username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com', 'nb_books' => 100],
+        2 => ['id' => 2, 'picture' => '/images/williamshekspear.jpeg', 'username' => 'William Shakespeare', 'email' => 'william.shakespeare@gmail.com', 'nb_books' => 200],
+        3 => ['id' => 3, 'picture' => '/images/taha hussein.jpge', 'username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300],
+    ];
 
-        return $this->render('author/index.html.twig', [
-            'authors' => $authors,
-        ]);
+    if (!isset($authors[$id])) {
+        throw $this->createNotFoundException('Author not found');
     }
+
+    $author = $authors[$id];
+
+    return $this->render('author/list.html.twig', [
+        'author' => $author,
+    ]);
+}
 }
